ea10c5d151cab9d6d0c89e6d13634711af12ce61
Dang Quang Huy <[nhocbehue270@gmail.com]>
2023-09-08 23:04:41 +0700

    add file sql

---
+++ backend/src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+(email, password, role, username)
+VALUES
+('admin1@gmail.com', '$2a$10$xnhNTFZXOyc7Fxz6MU2OHe5Bg9Am29HPG9ylWMW3YJuJ0ino6z6lK', 'ADMIN', 'admin1'),
+('user1@gmail.com', '$2a$10$M/VkBQVypVBkmxWyu8eRiu3pVL7O2kVvqD.AgXVDuEaKm1DGfC8kC', 'USER', 'user1');
